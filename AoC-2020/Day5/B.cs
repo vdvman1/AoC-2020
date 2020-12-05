bab596fd496c15a5ba4bae1bd43150d4e9da3dde
vdvman1 <vdvman88@gmail.com>
2020-12-06 10:04:11 +1100

    Day5 part 2

---
+++ AoC-2020/Day5/B.cs
@@ -0,0 +1,27 @@
+﻿using AoC_2020.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2020.Day5
+{
+    public class B
+    {
+        public async Task Run()
+        {
+            string[] lines = await File.ReadAllLinesAsync(Path.Combine("Day5", "input.txt"));
+            IEnumerable<(int, int)> ids = lines.Select(A.ToId).Where(id => id.HasValue).Select(id => id!.Value).OrderBy(id => id).Pairwise();
+            foreach ((int a, int b) in ids)
+            {
+                if(a + 1 != b)
+                {
+                    Console.WriteLine($"Boarding seat id: {a + 1}");
+                    return;
+                }
+            }
+        }
+    }
+}
--- AoC-2020/Program.cs
+++ AoC-2020/Program.cs
@@ -7,7 +7,7 @@ namespace AoC_2020
     {
         static async Task Main(string[] args)
         {
-            await new Day5.A().Run();
+            await new Day5.B().Run();
         }
     }
 }
