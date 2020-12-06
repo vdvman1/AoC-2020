5c10319ddecfa9ecbb8d8292d9e8958883513c10
vdvman1 <vdvman88@gmail.com>
2020-12-06 16:49:04 +1100

    Day6 part 1

---
+++ AoC-2020/Day6/A.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2020.Day6
+{
+    public class A
+    {
+        public async Task Run()
+        {
+            string[] lines = await File.ReadAllLinesAsync(Path.Combine("Day6", "input.txt"));
+            int count = 0;
+            for (int l = 0; l < lines.Length; l++)
+            {
+                var yesAnswers = new HashSet<char>();
+                while (l < lines.Length && !string.IsNullOrWhiteSpace(lines[l]))
+                {
+                    foreach (char c in lines[l])
+                    {
+                        yesAnswers.Add(c);
+                    }
+                    l++;
+                }
+                count += yesAnswers.Count;
+            }
+            Console.WriteLine($"Total number of unique yes answers: {count}");
+        }
+    }
+}
--- AoC-2020/Program.cs
+++ AoC-2020/Program.cs
@@ -7,7 +7,7 @@ namespace AoC_2020
     {
         static async Task Main(string[] args)
         {
-            await new Day5.B().Run();
+            await new Day6.A().Run();
         }
     }
 }
