179a238a7bfecf317349b939dda580bf2144e632
vdvman1 <vdvman88@gmail.com>
2020-12-14 11:01:16 +1100

    Add timing calculation

--- AoC-2020/Program.cs
+++ AoC-2020/Program.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace AoC_2020
 {
     class Program
     {
-        static Task Main(string[] args) => Day10.B.Run();
+        static async Task Main(string[] args)
+        {
+            var timer = Stopwatch.StartNew();
+            await Day10.B.Run();
+            timer.Stop();
+            Console.WriteLine($"Duration: {timer.ElapsedMilliseconds}ms");
+        }
     }
 }
