34235c23fd5dc6c89e211e9e3a5dd9db0c17b1f0
vdvman1 <vdvman88@gmail.com>
2020-12-14 14:06:53 +1100

    Add method to flatten a 2d array

---
+++ AoC-2020/Utilities/ArrayExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2020.Utilities
+{
+    public static class ArrayExtensions
+    {
+        public static IEnumerable<T> Flatten<T>(this T[,] values)
+        {
+            for (int i = 0; i < values.GetLength(0); i++)
+            {
+                for (int j = 0; j < values.GetLength(1); j++)
+                {
+                    yield return values[i, j];
+                }
+            }
+        }
+    }
+}
